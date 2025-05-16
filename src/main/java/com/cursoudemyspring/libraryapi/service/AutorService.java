34caff0cd778181288cc1d3166a3245cdb721426
Lucas Albuquerque <luucas.albuquerque23@gmail.com>
2025-05-16 01:31:32 -0300

    Feat: Udemy Study Project Update

---
+++ src/main/java/com/cursoudemyspring/libraryapi/service/AutorService.java
@@ -0,0 +1,21 @@
+package com.cursoudemyspring.libraryapi.service;
+
+import com.cursoudemyspring.libraryapi.controller.AutorController;
+import com.cursoudemyspring.libraryapi.model.Autor;
+import com.cursoudemyspring.libraryapi.repository.AutorRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AutorService {
+
+    private final AutorRepository autorRepository;
+
+    public AutorService(AutorRepository autorRepository) {
+        this.autorRepository = autorRepository;
+    }
+
+    public Autor salvar(Autor autor){
+        return autorRepository.save(autor);
+    }
+
+}
