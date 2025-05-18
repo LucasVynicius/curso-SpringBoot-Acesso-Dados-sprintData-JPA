9cd79d3313e9f65d74f61b5e691a93143fd5e528
Lucas Albuquerque <luucas.albuquerque23@gmail.com>
2025-05-18 12:07:27 -0300

    Feat: Pesquisa de Autores com filtro

--- src/main/java/com/cursoudemyspring/libraryapi/controller/AutorController.java
+++ src/main/java/com/cursoudemyspring/libraryapi/controller/AutorController.java
@@ -3,6 +3,7 @@ package com.cursoudemyspring.libraryapi.controller;
 import com.cursoudemyspring.libraryapi.dto.AutorDTO;
 import com.cursoudemyspring.libraryapi.model.Autor;
 import com.cursoudemyspring.libraryapi.service.AutorService;
+import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,8 +11,10 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/autores")
@@ -49,6 +52,22 @@ public class AutorController {
        }
 
        return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AutorDTO>> pesquisar(
+            @RequestParam(value = "nome", required = false) String nome,
+            @RequestParam(value = "nacionalidade", required = false) String nacionalidade){
+
+        List<Autor> resultado = autorService.pesquisa(nome, nacionalidade);
+        List<AutorDTO> lista = resultado.stream().map(autor -> new AutorDTO(
+                autor.getId(),
+                autor.getNome(),
+                autor.getData_nascimento(),
+                autor.getNacionalidade()))
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(lista);
     }
 
     @DeleteMapping("/{id}")
