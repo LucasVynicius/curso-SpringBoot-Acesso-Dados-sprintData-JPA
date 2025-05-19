8bbd50a19ad324f792c7fcb3a3f157630a3ed3de
Lucas Vynicius Gonçalves Albuquerque <lucas.a@SUDEMAPB.INTRA>
2025-05-19 13:32:32 -0300

    Feat: Melhorando a pesquisa de autores com Query By Example

--- src/main/java/com/cursoudemyspring/libraryapi/dto/AutorDTO.java
+++ src/main/java/com/cursoudemyspring/libraryapi/dto/AutorDTO.java
@@ -3,6 +3,8 @@ package com.cursoudemyspring.libraryapi.dto;
 import com.cursoudemyspring.libraryapi.model.Autor;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 import java.util.UUID;
@@ -10,10 +12,15 @@ import java.util.UUID;
 public record AutorDTO(
         UUID uuid,
         @NotBlank(message = "Campo obrigatorio")
+        @Size(min = 50, max = 130, message = "Campo fora do tamanho padrão")
         String nome,
+
         @NotNull(message = "Campo obrigatorio")
+        @Past(message = "Não pode ser uma data futura")
         LocalDate dataNascimento,
+
         @NotBlank(message = "Campo obrigatorio")
+        @Size(max = 50, min = 10, message = "Campo fora do tamanho padrão")
         String nacionalidade
 ) {
     public Autor dadosAutor(){
